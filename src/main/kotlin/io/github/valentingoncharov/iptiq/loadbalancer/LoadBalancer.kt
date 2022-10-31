c3e748321fd399581d093c5a07bb8f7738028c44
Valentin Goncharov <v.goncharov@quin.md>
2022-10-31 16:31:28 +0100

    #2:  - add registry into LoadBalancer as storage of active providers  - introduced method get() for LoadBalancer  - created unit tests

--- src/main/kotlin/io/github/valentingoncharov/iptiq/loadbalancer/LoadBalancer.kt
+++ src/main/kotlin/io/github/valentingoncharov/iptiq/loadbalancer/LoadBalancer.kt
@@ -1,21 +1,37 @@
 package io.github.valentingoncharov.iptiq.loadbalancer
 
+import io.github.valentingoncharov.iptiq.loadbalancer.registry.Registry
 import io.github.valentingoncharov.iptiq.provider.Provider
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.withContext
 
 const val MAXIMUM_LOAD_BALANCER_CAPACITY = 10
+
+@OptIn(DelicateCoroutinesApi::class)
 class LoadBalancer(
+    private val registry: Registry<Provider>,
     private val capacity: Int = MAXIMUM_LOAD_BALANCER_CAPACITY
 ) {
+
+    private val loadBalancerContext = newSingleThreadContext("Load-Balancer-Context")
+
     private val registeredProviders = mutableMapOf<String, Provider>()
 
-    fun register(provider: Provider) = registeredProviders[provider.id]
-        ?. let { false }
-        ?: let {
-            if (registeredProviders.size < capacity) {
-                registeredProviders[provider.id] = provider
-                true
+    suspend fun get(): String = registry.next().get()
+
+    suspend fun register(provider: Provider) = withContext(loadBalancerContext) {
+        registeredProviders.putIfAbsent(provider.id, provider)
+        addToRegistry(provider.id)
+    }
+
+    private suspend fun addToRegistry(providerId: String) = withContext(loadBalancerContext) {
+        registeredProviders[providerId] ?. let {provider ->
+            if (registry.size < capacity) {
+                registry.add(provider)
             } else {
                 false
             }
-        }
+        } ?: false
+    }
 }
--- src/test/kotlin/io/github/valentingoncharov/iptiq/loadbalancer/LoadBalancerTest.kt
+++ src/test/kotlin/io/github/valentingoncharov/iptiq/loadbalancer/LoadBalancerTest.kt
@@ -1,39 +1,75 @@
 package io.github.valentingoncharov.iptiq.loadbalancer
 
+import io.github.valentingoncharov.iptiq.loadbalancer.registry.RandomRegistry
 import io.github.valentingoncharov.iptiq.provider.Provider
+import io.mockk.coVerify
+import io.mockk.spyk
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
-
+@OptIn(ExperimentalCoroutinesApi::class)
 internal class LoadBalancerTest {
 
     @Test
     fun `should register new provider`() {
-        val loadBalancer = LoadBalancer()
-        assertThat(loadBalancer.register(Provider())).isTrue
+        val registry = spyk(RandomRegistry<Provider>())
+        val loadBalancer = LoadBalancer(registry)
+        runTest {
+            val provider = Provider("Test 1")
+            assertThat(loadBalancer.register(provider)).isTrue
+            coVerify { registry.add(provider) }
+        }
     }
 
     @Test
     fun `should register defined number of providers`() {
-        val loadBalancer = LoadBalancer(2)
-        assertThat(loadBalancer.register(Provider())).isTrue
-        assertThat(loadBalancer.register(Provider())).isTrue
-        assertThat(loadBalancer.register(Provider())).isFalse
+        val loadBalancer = LoadBalancer(registry = RandomRegistry(), capacity = 2)
+        runTest {
+            assertThat(loadBalancer.register(Provider())).isTrue
+            assertThat(loadBalancer.register(Provider())).isTrue
+            assertThat(loadBalancer.register(Provider())).isFalse
+        }
     }
 
     @Test
     fun `should register default number of providers`() {
-        val loadBalancer = LoadBalancer()
-        assertThat(loadBalancer.register(Provider())).isTrue
-        assertThat(loadBalancer.register(Provider())).isTrue
-        assertThat(loadBalancer.register(Provider())).isTrue
-        assertThat(loadBalancer.register(Provider())).isTrue
-        assertThat(loadBalancer.register(Provider())).isTrue
-        assertThat(loadBalancer.register(Provider())).isTrue
-        assertThat(loadBalancer.register(Provider())).isTrue
-        assertThat(loadBalancer.register(Provider())).isTrue
-        assertThat(loadBalancer.register(Provider())).isTrue
-        assertThat(loadBalancer.register(Provider())).isTrue
-        assertThat(loadBalancer.register(Provider())).isFalse
+        val loadBalancer = LoadBalancer(RandomRegistry())
+        runTest {
+            assertThat(loadBalancer.register(Provider())).isTrue
+            assertThat(loadBalancer.register(Provider())).isTrue
+            assertThat(loadBalancer.register(Provider())).isTrue
+            assertThat(loadBalancer.register(Provider())).isTrue
+            assertThat(loadBalancer.register(Provider())).isTrue
+            assertThat(loadBalancer.register(Provider())).isTrue
+            assertThat(loadBalancer.register(Provider())).isTrue
+            assertThat(loadBalancer.register(Provider())).isTrue
+            assertThat(loadBalancer.register(Provider())).isTrue
+            assertThat(loadBalancer.register(Provider())).isTrue
+            assertThat(loadBalancer.register(Provider())).isFalse
+        }
+    }
+
+    @Test
+    fun `should get next provider from registry and call get method`() {
+        val providerId1 = "Test 1"
+        val providerId2 = "Test 2"
+        val providerId3 = "Test 3"
+        val registry = spyk(RandomRegistry<Provider>())
+        val loadBalancer = LoadBalancer(registry)
+        runTest {
+            assertThat(loadBalancer.register(Provider(providerId1))).isTrue
+            assertThat(loadBalancer.register(Provider(providerId2))).isTrue
+            assertThat(loadBalancer.register(Provider(providerId3))).isTrue
+            assertThat(registry.size).isEqualTo(3)
+
+            repeat(10) {
+                assertThat(loadBalancer.get()).isIn(providerId1, providerId2, providerId3)
+            }
+
+            coVerify(exactly = 10) { registry.next() }
+        }
     }
 }
