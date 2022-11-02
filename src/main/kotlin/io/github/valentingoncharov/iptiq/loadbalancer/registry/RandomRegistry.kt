e78f61b54437a3a636692e8d7889398ab95e3307
Valentin Goncharov <goncharov.valentin@gmail.com>
2022-11-02 10:16:28 +0100

    #0:  - tidy up

--- src/main/kotlin/io/github/valentingoncharov/iptiq/loadbalancer/registry/RandomRegistry.kt
+++ src/main/kotlin/io/github/valentingoncharov/iptiq/loadbalancer/registry/RandomRegistry.kt
@@ -3,8 +3,6 @@ package io.github.valentingoncharov.iptiq.loadbalancer.registry
 import kotlin.random.Random
 
 class RandomRegistry<T>: AbstractRegistry<T>() {
-    override val size: Int
-        get() = super.size
 
     override fun nextIndex(): Int = Random.nextInt(registry.size)
 }
